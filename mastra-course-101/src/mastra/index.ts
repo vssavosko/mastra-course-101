cebee6deb4bbce23deac8fa899f84a773978e785
Vladislav Savosko <vladislav.savosko@smile2impress.com>
2025-08-10 20:31:38 +0200

    mc101: export agent

--- mastra-course-101/src/mastra/index.ts
+++ mastra-course-101/src/mastra/index.ts
@@ -3,10 +3,11 @@ import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows/weather-workflow';
 import { weatherAgent } from './agents/weather-agent';
+import { financialAgent } from './agents/financial-agent';
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
-  agents: { weatherAgent },
+  agents: { weatherAgent, financialAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ':memory:',
